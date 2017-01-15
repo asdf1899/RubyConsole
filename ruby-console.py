634f1c9e7969bfeacae8a6eae7f947526fcaa09d
asdf1899 <araid.anas99@gmail.com>
2017-01-15 21:49:17 +0100

    Add readConfig function

--- ruby-console.py
+++ ruby-console.py
@@ -1,6 +1,16 @@
 #-*- coding: UTF-8 -*-
 
+def main():
+    PATH = readConfig()
 
+
+def readConfig():
+    f = open("config.txt")
+    for ff in f.readlines():
+        file= ff.strip()
+    if file != file + "/":
+        file= file+ "/"
+    return file
 
 if __name__ == '__main__':
     main()
